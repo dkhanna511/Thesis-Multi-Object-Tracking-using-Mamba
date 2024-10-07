69708ff24f0029814e2e3c6c1206c2f9236a570f
viplab <khannaaiig@gmail.com>
2024-10-07 15:56:50 -0400

    Uncommented the evaluation code, right now its not giving great results

--- tools/run_mamba_tracker.py
+++ tools/run_mamba_tracker.py
@@ -66,9 +66,13 @@ def main(exp, args, num_gpu):
     if args.tsize is not None:
         exp.test_size = (args.tsize, args.tsize)
 
+
+    print(" model name is : ", args.model_type)
+    # exit(0)
     model = exp.get_model()
     # print(" model is : ", model) ### i guess this takes in the YOLOX model to give detections
-    
+    print("image size mentioned is ", exp.test_size)
+    # exit(0)
     logger.info("Model Summary: {}".format(get_model_info(model, exp.test_size)))
 
     val_loader = exp.get_eval_loader(args.batch_size, is_distributed, args.test)
@@ -123,9 +127,10 @@ def main(exp, args, num_gpu):
     # print("gt files are :", gtfiles)
     # start tracking
     
+    model_type = args.model_type
     # exit(0)
     *_, summary = evaluator.evaluate_mamba_track(
-            model, is_distributed, args.fp16, trt_file, decoder, exp.test_size, results_folder
+            model, is_distributed, args.fp16, trt_file, decoder, exp.test_size, results_folder, model_type = model_type
     )
     
     # if args.test:
@@ -133,47 +138,47 @@ def main(exp, args, num_gpu):
     #     return 
 
     # # if we evaluate on validation set, 
-    # logger.info("\n" + summary)
+    logger.info("\n" + summary)
 
     # # evaluate on the validation set
-    # mm.lap.default_solver = 'lap'
-    # gtfiles = glob.glob(os.path.join('datasets/dancetrack/val', '*/gt/gt.txt'))
-    # print('gt_files', gtfiles)
-    # tsfiles = [f for f in glob.glob(os.path.join(results_folder, '*.txt')) if not os.path.basename(f).startswith('eval')]
+    mm.lap.default_solver = 'lap'
+    gtfiles = glob.glob(os.path.join('datasets/dancetrack/val', '*/gt/gt.txt'))
+    print('gt_files', gtfiles)
+    tsfiles = [f for f in glob.glob(os.path.join(results_folder, '*.txt')) if not os.path.basename(f).startswith('eval')]
 
-    # logger.info('Found {} groundtruths and {} test files.'.format(len(gtfiles), len(tsfiles)))
-    # logger.info('Available LAP solvers {}'.format(mm.lap.available_solvers))
-    # logger.info('Default LAP solver \'{}\''.format(mm.lap.default_solver))
-    # logger.info('Loading files.')
+    logger.info('Found {} groundtruths and {} test files.'.format(len(gtfiles), len(tsfiles)))
+    logger.info('Available LAP solvers {}'.format(mm.lap.available_solvers))
+    logger.info('Default LAP solver \'{}\''.format(mm.lap.default_solver))
+    logger.info('Loading files.')
     
-    # gt = OrderedDict([(Path(f).parts[-3], mm.io.loadtxt(f, fmt='mot15-2D', min_confidence=1)) for f in gtfiles])
-    # ts = OrderedDict([(os.path.splitext(Path(f).parts[-1])[0], mm.io.loadtxt(f, fmt='mot15-2D', min_confidence=-1)) for f in tsfiles])    
+    gt = OrderedDict([(Path(f).parts[-3], mm.io.loadtxt(f, fmt='mot15-2D', min_confidence=1)) for f in gtfiles])
+    ts = OrderedDict([(os.path.splitext(Path(f).parts[-1])[0], mm.io.loadtxt(f, fmt='mot15-2D', min_confidence=-1)) for f in tsfiles])    
     
-    # mh = mm.metrics.create()    
-    # accs, names = compare_dataframes(gt, ts)
+    mh = mm.metrics.create()    
+    accs, names = compare_dataframes(gt, ts)
     
-    # logger.info('Running metrics')
-    # metrics = ['recall', 'precision', 'num_unique_objects', 'mostly_tracked',
-    #            'partially_tracked', 'mostly_lost', 'num_false_positives', 'num_misses',
-    #            'num_switches', 'num_fragmentations', 'mota', 'motp', 'num_objects']
-    # summary = mh.compute_many(accs, names=names, metrics=metrics, generate_overall=True)
-    # div_dict = {
-    #     'num_objects': ['num_false_positives', 'num_misses', 'num_switches', 'num_fragmentations'],
-    #     'num_unique_objects': ['mostly_tracked', 'partially_tracked', 'mostly_lost']}
-    # for divisor in div_dict:
-    #     for divided in div_dict[divisor]:
-    #         summary[divided] = (summary[divided] / summary[divisor])
-    # fmt = mh.formatters
-    # change_fmt_list = ['num_false_positives', 'num_misses', 'num_switches', 'num_fragmentations', 'mostly_tracked',
-    #                    'partially_tracked', 'mostly_lost']
-    # for k in change_fmt_list:
-    #     fmt[k] = fmt['mota']
-    # print(mm.io.render_summary(summary, formatters=fmt, namemap=mm.io.motchallenge_metric_names))
+    logger.info('Running metrics')
+    metrics = ['recall', 'precision', 'num_unique_objects', 'mostly_tracked',
+               'partially_tracked', 'mostly_lost', 'num_false_positives', 'num_misses',
+               'num_switches', 'num_fragmentations', 'mota', 'motp', 'num_objects']
+    summary = mh.compute_many(accs, names=names, metrics=metrics, generate_overall=True)
+    div_dict = {
+        'num_objects': ['num_false_positives', 'num_misses', 'num_switches', 'num_fragmentations'],
+        'num_unique_objects': ['mostly_tracked', 'partially_tracked', 'mostly_lost']}
+    for divisor in div_dict:
+        for divided in div_dict[divisor]:
+            summary[divided] = (summary[divided] / summary[divisor])
+    fmt = mh.formatters
+    change_fmt_list = ['num_false_positives', 'num_misses', 'num_switches', 'num_fragmentations', 'mostly_tracked',
+                       'partially_tracked', 'mostly_lost']
+    for k in change_fmt_list:
+        fmt[k] = fmt['mota']
+    print(mm.io.render_summary(summary, formatters=fmt, namemap=mm.io.motchallenge_metric_names))
 
-    # metrics = mm.metrics.motchallenge_metrics + ['num_objects']
-    # summary = mh.compute_many(accs, names=names, metrics=metrics, generate_overall=True)
-    # print(mm.io.render_summary(summary, formatters=mh.formatters, namemap=mm.io.motchallenge_metric_names))
-    # logger.info('Completed')
+    metrics = mm.metrics.motchallenge_metrics + ['num_objects']
+    summary = mh.compute_many(accs, names=names, metrics=metrics, generate_overall=True)
+    print(mm.io.render_summary(summary, formatters=mh.formatters, namemap=mm.io.motchallenge_metric_names))
+    logger.info('Completed')
 
 
 if __name__ == "__main__":
